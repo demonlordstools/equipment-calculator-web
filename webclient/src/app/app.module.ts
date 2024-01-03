ad3838990dd2bd017a62b67ef2abb8544a52dc4b
demonlordstools <97765266+demonlordstools@users.noreply.github.com>
2024-01-03 14:36:09 +0100

    Update dependencies (#29)
    * update dependencies
    * update to Angular 17
    * update github ci/cd workflows to use NodeJs 20.x

--- webclient/src/app/app.module.ts
+++ webclient/src/app/app.module.ts
@@ -1,31 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSelectModule } from '@angular/material/select';
-import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-import { ContextComponent } from './_components/context/context.component';
-import { EquipmentCalculatorComponent } from './_components/equipment-calculator/equipment-calculator.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BaseDataInputComponent } from './_components/equipment-calculator/_components/base-data-input/base-data-input.component';
-import { StatWeightingComponent } from './_components/equipment-calculator/_components/stat-weighting/stat-weighting.component';
-import { EquipmentSetComponent } from './_components/equipment-calculator/_components/equipment-set/equipment-set.component';
-import { HelpDialogComponent } from './_components/help-dialog/help-dialog.component';
-import { AboutDialogComponent } from './_components/about-dialog/about-dialog.component';
 import { AppComponent } from './app.component';
+import { AboutDialogComponent } from './_components/about-dialog/about-dialog.component';
+import { HelpDialogComponent } from './_components/help-dialog/help-dialog.component';
+import { EquipmentSetComponent } from './_components/equipment-calculator/_components/equipment-set/equipment-set.component';
+import { StatWeightingComponent } from './_components/equipment-calculator/_components/stat-weighting/stat-weighting.component';
+import { BaseDataInputComponent } from './_components/equipment-calculator/_components/base-data-input/base-data-input.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EquipmentCalculatorComponent } from './_components/equipment-calculator/equipment-calculator.component';
+import { ContextComponent } from './_components/context/context.component';
 
 @NgModule({
     declarations: [
