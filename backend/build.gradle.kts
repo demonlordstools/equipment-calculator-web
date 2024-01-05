cbdef6ae0ce2cc1103c6c90c977d242bbf8c8a38
demonlordstools <97765266+demonlordstools@users.noreply.github.com>
2024-01-05 15:12:08 +0100

    Update build.gradle.kts
    remove unwanted whitespace

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    }
+}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
